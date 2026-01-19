da19c029bb255b1da1fad1e6f72bf26ada1365b3
Browndog7912 <jakevento41704@gmail.com>
2026-01-18 20:25:13 -0800

    Update diskmaker.lua

--- installer/diskmaker.lua
+++ installer/diskmaker.lua
@@ -2,7 +2,8 @@ if fs.exists("/disk/") then
     print("Installing...")
     shell.run("wget https://raw.githubusercontent.com/JV-lab-create/Aurora-OS/refs/heads/main/disk/startup.lua /disk/startup.lua")
     shell.run("wget https://github.com/JV-lab-create/Aurora-OS/blob/main/disk/installer.script.lua /disk/installer.script.lua")
-    shell.run("wget https://raw.githubusercontent.com/JV-lab-create/Aurora-OS/refs/heads/main/disk/basalt.lua /disk/basalt.lua")
+    --shell.run("wget https://raw.githubusercontent.com/JV-lab-create/Aurora-OS/refs/heads/main/disk/basalt.lua /disk/basalt.lua")
+    -- you must make a diffrent disk for the basalt
 else
     print("Please Insert disk to make install disk")
 end
